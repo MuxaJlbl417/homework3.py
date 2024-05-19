decb88ea40ca35711388d07a5ba1253abe2df83f
MuxaJlbl417 <sudakov170389@gmail.com>
2024-05-19 20:10:52 +0500

    Initial commit

---
+++ homework3.py
@@ -0,0 +1,17 @@
+my_string = input('Введите текст ')
+print(len(my_string))
+
+my_string = input('Введите текст ')
+print(my_string.upper())
+
+my_string = input('Введите текст ')
+print(my_string.lower())
+
+my_string = input('Введите текст ')
+print(my_string.replace(' ', ''))
+
+my_string = input('Введите текст ')
+print(my_string[0])
+
+my_string = input('Введите текст ')
+print(my_string[-1])
